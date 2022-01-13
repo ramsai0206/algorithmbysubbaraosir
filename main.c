93f0d9fc48571ab87e4886a38aea804a6bfb6331
ramsai0206 <43134618+ramsai0206@users.noreply.github.com>
2022-01-13 23:59:52 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,80 +1,81 @@
-//
-//  main.c
-//  aInterb
-//
-//  Created by Ramsai Kamavaram on 12/01/22.
-//
 
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc, const char * argv[]) {
-    int n1,n2,p=0,q=0;
-    printf("enter the size of first array\n");
-    scanf("%d",&n1);
-    printf("enter the size of second array\n");
-    scanf("%d",&n2);
-    int *a=(int*)malloc(sizeof(int)*n1);
-    int *b=(int*)malloc(sizeof(int)*n2);
-    printf("enter elemets of first array");
-    for(int i=0;i<n1;i++)
-    {
-        printf("enter %d element",i+1);
-        scanf("%d",&a[i]);
-    }
-    printf("enter elemets of second array");
-    for(int i=0;i<n2;i++)
-    {
-        printf("enter %d element",i+1);
-        scanf("%d",&b[i]);
-    }
-    printf("A intersection B = \n");
-    while((p<n1)&&(q<n2))
-    {
-        if(a[p]<b[q])
-        {
-            p++;
-        }
-        else if(a[p]>b[q])
-        {
-            q++;
-        }
-        else
-        {
-            printf("%d ",a[p]);
-            p++;
-            q++;
-        }
-    }
-    printf("\n union of A U B = \n");
-    p=0;q=0;
-    while((p<n1)&&(q<n2))
-    {
-        if(a[p]<b[q])
-        {
-            printf("%d ",a[p]);
-            p++;
-        }
-        else if(a[p]>b[q])
-        {
-            printf("%d ",b[q]);
-            q++;
-        }
-        else
-        {
-            printf("%d ",a[p]);
-            p++;
-            q++;
-        }
-    }
-    while(p<n1)
-    {
-        printf("%d ",a[p++]);
-    }
-    while(q<n2)
-    {
-        printf("%d ",b[q++]);
-    }
+#include<stdio.h>
+
+void merge(int arr[],int start,int mid,int end)//p indicates the starting index and q indicates till where we divided and r indicates the last index
+{
     
-    
-    return 0;
+    int n1 = mid - start + 1;//number of elements till the first half
+  int n2 = end - mid;//number of elements in the second half
+
+  int fM[n1], sM[n2];//we store the first half in L array and second half in M array
+
+  for (int i = 0; i < n1; i++)
+    fM[i] = arr[start + i];
+  for (int j = 0; j < n2; j++)
+    sM[j] = arr[mid + 1 + j];
+
+  // Maintain current index of sub-arrays and main array
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = start;
+
+  // Until we reach either end of either L or M, pick larger among
+  // elements L and M and place them in the correct position at A[p..r]
+  while (i < n1 && j < n2) {
+    if (fM[i] <= sM[j]) {
+      arr[k] = fM[i];
+      i++;
+    } else {
+      arr[k] = sM[j];
+      j++;
+    }
+    k++;
+  }
+
+  // When we run out of elements in either L or M,
+  // pick up the remaining elements and put in A[p..r]
+  while (i < n1) {
+    arr[k] = fM[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = sM[j];
+    j++;
+    k++;
+  }
+}
+void mergeSort(int arr[],int start,int end)//start indicates the starting index of the array and end indicates ending address of array
+{
+   // in this we divided if atleast more than 2 elements
+    if(start < end)
+    {
+        int mid=(start + end) / 2;
+       mergeSort(arr,start,mid);//this will recursively call it self by taking first half of the array
+       mergeSort(arr,mid+1,end);//this will recursively call it self by taking second half of the array
+       merge(arr,start,mid,end);// this will be called when we need to combine 2 subarrays
+    }
+}
+void printsort(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d,",arr[i]);
+    }
+}
+int main()
+{
+ int n;
+ printf("enter the size of array\n");
+ scanf("%d",&n);
+ int a[n];
+ for(int i=0;i<n;i++)
+ {
+     scanf("%d",&a[i]);
+ }
+ mergeSort(a,0,n-1);
+ printf("sorted array");
+ printsort(a,n);
 }
